cc78f935c22ff48222a4da300190a4d97e9e6cee
SangHyeon Lee <67571350+Hys-Lee@users.noreply.github.com>
2024-10-24 18:16:47 +0900

    init: webpack-dev-server 사용하는 세팅

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        include: [path.resolve(__dirname, 'src/')],
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  devtool: 'source-map',
+  mode: 'development',
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    port: 8000,
+    hot: true,
+  },
+};
